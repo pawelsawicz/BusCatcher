7f614ff9adb4ca94c7e5de586938ffeb9c16206f
Pawel Sawicz <pawel@sawicz.eu>
2013-07-20 21:55:26 +0200

    Project was created

---
+++ BusCatcher.Core/BusCatcher.Core/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using BusCatcher.Core.Resources;
+
+namespace BusCatcher.Core
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
