f75b75836a22338cfc352b91cd46d707f88ca9aa
Pawel Sawicz <pawel@sawicz.eu>
2013-08-01 20:43:35 +0200

    created spash screen, small - medium tiles

---
+++ BusCatcher.Core/BusCatcher.Core/Views/Common/ExtendedSplashScreen.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace BusCatcher.Core.Views.Common
+{
+    public partial class ExtendedSplashScreen : PhoneApplicationPage
+    {
+        public ExtendedSplashScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
